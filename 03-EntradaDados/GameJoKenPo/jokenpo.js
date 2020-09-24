b8fe6ed3465f91e198d3a8c5f2bc91c8bad6219f
Leonardo <leonardo.viana@armateus.com.br>
2020-09-24 17:47:33 -0300

    Ajuste de comentários no código

--- 03-EntradaDados/GameJoKenPo/jokenpo.js
+++ 03-EntradaDados/GameJoKenPo/jokenpo.js
@@ -1,11 +1,10 @@
-//Crie um game de jo-ken-po. A cada rodada, o jogador vê o menu:
+/*Crie um game de jo-ken-po. A cada rodada, o jogador vê o menu:
+Escolha sua jogada: 1 - Papel 2 - Pedra 3 - Tesoura
+O jogo lê a opção do jogador e verifica se é válida.
+Se for inválida, o jogador perde a rodada e o jogo acaba. Se for válida, o computador escolhe uma resposta aleatória, que é mostrada ao jogador. 
+Se o jogador ganhar, ele pode jogar mais uma rodada e sua pontuação aumenta. O jogo acaba quando o jogador perde uma rodada. 
+A pontuação total é mostrada no fim do jogo.*/
 
-// Escolha sua jogada: 1 - Papel 2 - Pedra 3 - Tesoura
-
-// O jogo lê a opção do jogador e verifica se é válida.
-// Se for inválida, o jogador perde a rodada e o jogo acaba. Se for válida, o computador escolhe uma resposta aleatória, que é mostrada ao jogador. 
-// Se o jogador ganhar, ele pode jogar mais uma rodada e sua pontuação aumenta. O jogo acaba quando o jogador perde uma rodada. 
-// A pontuação total é mostrada no fim do jogo.
 var readlineSync = require('readline-sync');
 console.log("Game de jo-ken-po ");
 var opcoes = ["Papel", "Pedra", "Tesoura"];
