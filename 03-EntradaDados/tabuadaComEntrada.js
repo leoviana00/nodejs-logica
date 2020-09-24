559119d917439774183765fe0eeb6f8a69614238
Leonardo <leonardo.viana@armateus.com.br>
2020-09-24 17:57:11 -0300

    Tabuada

---
+++ 03-EntradaDados/tabuadaComEntrada.js
@@ -0,0 +1,19 @@
+// Exercício: Programa que gera a tabuada do número 2
+// Armazenado na variável "n"
+// Entrada de dados pelo o usuário
+var readlineSync = require('readline-sync');
+console.log("Programa que exibe a tabuada de um número ");
+var n = readlineSync.question('Digite o valor desejado: ');
+console.log("Tabuada do número "+ n);
+console.log(n +" X 0 = " + (n*0));
+console.log(n +" X 1 = " + (n*1));
+console.log(n +" X 2 = " + (n*2));
+console.log(n +" X 3 = " + (n*3));
+console.log(n +" X 4 = " + (n*4));
+console.log(n +" X 5 = " + (n*5));
+console.log(n +" X 6 = " + (n*6));
+console.log(n +" X 7 = " + (n*7));
+console.log(n +" X 8 = " + (n*8));
+console.log(n +" X 9 = " + (n*9));
+console.log(n +" X 10 = " + (n*10));
+
