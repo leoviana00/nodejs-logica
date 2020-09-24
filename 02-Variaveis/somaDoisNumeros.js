c7e72c014c1dc01b2932ba303aeefd0d2ad77abd
Leonardo <leonardo.viana@armateus.com.br>
2020-09-24 14:33:27 -0300

    Soma dois números utilizando constantes

---
+++ 02-Variaveis/somaDoisNumeros.js
@@ -0,0 +1,10 @@
+/*Programa que
+soma dois números*/
+
+var n1 = 2;
+var n2 = 3;
+var total = 0;
+
+total = n1 + n2;
+console.log("Programa para somar dois números");
+console.log (n1 + " + " + n2 + " = " + total);
