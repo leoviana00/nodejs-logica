52260bc473962e34b36267e42c74ba0349d9adb6
Leonardo <leonardo.viana@armateus.com.br>
2020-09-25 13:55:59 -0300

    Programa que verifica se um número é par ou impar

---
+++ 04-ComandoIF/ParOuImpar/index.js
@@ -0,0 +1,17 @@
+//Programa para verificar se um número é par ou impar
+var readlineSync = require('readline-sync');
+var n = 0;
+var resto = 0;
+
+console.log("Programa que verifica se um número é par ou impar");
+n = parseInt(readlineSync.question("Digite um número inteiro :"));
+resto = n%2;
+
+if (resto == 0){
+    console.log("Este é um número par");
+}
+else{
+    console.log("Este é um número impar");
+}
+
+
