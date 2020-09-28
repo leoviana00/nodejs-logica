40514fec7b64221fb1407393d8843e3286331584
Leonardo <leonardo.viana@armateus.com.br>
2020-09-28 09:23:40 -0300

    Utilizando while para contar 10x

---
+++ 05-LacosRepeticao/ContaAteDez/index.js
@@ -0,0 +1,28 @@
+// Programa que lé Alô mundo 10 x
+/*
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+console.log("Alô Mundo !!!");
+*/
+
+// Utilizando while
+// Armazenado valor de i
+console.log("Programa que lê olá mundo 10 x");
+
+var i = 1;
+//Enquanto i for menor igual a 10 vai executar o bloco
+
+while( i <= 10){
+    console.log("Alô Mundo !!!");
+    // variável i recebe o valor de i + 1
+    i = i + 1;
+}
+    console.log("Fim do programa");
+
