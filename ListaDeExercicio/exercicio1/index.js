c26c9cb39a8f054cf02c3b5f739090613f9b69f4
Leonardo Viana <leonardoviana00@hotmail.com>
2020-10-05 10:41:28 -0300

    Organização e comentário no código

--- ListaDeExercicio/exercicio1/index.js
+++ ListaDeExercicio/exercicio1/index.js
@@ -1,7 +1,7 @@
 /* 
 Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
 mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
-(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
+(Ex: 3 anos, 2 meses e 15 dias.)
 */
 //Biblioteca reposável por ler a entrada de dados
 const readlineSync = require('readline-sync');
